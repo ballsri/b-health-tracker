4e5b1fd9982c524c71a399f582552573aad2a547
ballsri <ball04407@hotmail.com>
2024-01-03 22:40:47 +0700

    feat: navbar dropdown

--- b-health-tracker/src/components/navbar/Navbar.tsx
+++ b-health-tracker/src/components/navbar/Navbar.tsx
@@ -1,10 +1,39 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
-import Image from "next/image";
+import NavbarDropdownIcon from "../icon/navbar-dropdown";
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+    setIsClicked(!isClicked);
+  };
+  // Reference to the dropdown menu
+  const buttonRef = useRef(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: { target: any }) => {
+      // If the click is outside the dropdownRef element, close the dropdown
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setIsClicked(false);
+        setIsOpen(false);
+      }
+    };
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
     <nav className="bg-gray-800 text-white p-4">
@@ -15,19 +44,39 @@ export const Navbar = () => {
           </Link>
         </div>
         <div className="hidden md:flex space-x-4">
-          <Link className="hover:text-gray-300" href="/">
-            หน้าแรก
+          <Link className="hover:text-gray-300" href="/dashboard">
+            ลงข้อมูลรายวัน
           </Link>
           <Link className="hover:text-gray-300" href="/dashboard">
             ข้อมูลของทุกคน
           </Link>
         </div>
         <button
-          className="md:hidden focus:outline-none"
+          className={`md:hidden focus:outline-none hover:bg-gray-500 hover:bg-opacity-25 p-1 rounded-full transition duration-150 ease-in-out ${
+            isClicked
+              ? "bg-gray-600 hover:bg-gray-500"
+              : "hover:bg-gray-500 hover:bg-opacity-25"
+          } p-1 rounded-full transition duration-150 ease-in-out`}
           onClick={toggleDropdown}
+          ref={buttonRef}
         >
-          <Image src="/navbar-dropdown.svg" alt="Dropdown" className=""  width={32} height={32}/>
+          <NavbarDropdownIcon className="w-6 h-6" />
         </button>
+
+        <div
+          className={`absolute top-14 right-0 w-48 bg-gray-800 text-white flex flex-col items-start py-2 px-4 md:hidden ${
+            isOpen ? "block" : "hidden"
+          }`}
+          ref={dropdownRef}
+        >
+          <Link href="/about" className="hover:text-gray-300 py-1">
+            ลงข้อมูลรายวัน
+          </Link>
+          <Link href="/contact" className="hover:text-gray-300 py-1">
+            ข้อมูลของทุกคน
+          </Link>
+          {/* Add more links as needed */}
+        </div>
       </div>
     </nav>
   );
